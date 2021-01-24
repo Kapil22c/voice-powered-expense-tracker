b0bc275f2179307d2f1af79ec75772b1f1c8bd9d
Kapil bavisiya <kapilbavisiya555@gmail.com>
2021-01-25 00:15:49 +0530

    updated

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,31 +1,43 @@
-import React from 'react';
-import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
-import useStyles from './styles';
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
+import useStyles from "./styles";
+import Form from "./Form/Form";
 
 const Main = () => {
-    const classes = useStyles();
-    return (
-        <Card className={classes.roor}>
-            <CardHeader title="Expense" subheader="Powered by speechly" />
-            <CardContent>
-                <Typography align="center" variant="h5">Total Balance ₹5000</Typography>
-                <Typography variant="subtitle1" style={{lineHeight:'1.5em', marginTop:'20px'}}>
-                    {/* InfoCard */}
-                    Try saying: Add income for ₹500 in category Salary for Monday...
-                </Typography>
-                <Divider /> 
-                {/* Form */}
-            </CardContent>
-            <CardContent className={classes.cartContent}>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        {/* List */}
-                    </Grid>
-                </Grid>
-            </CardContent>
-
-        </Card>
-    )
-}
+  const classes = useStyles();
+  return (
+    <Card className={classes.roor}>
+      <CardHeader title="Expense" subheader="Powered by speechly" />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance ₹5000
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: "1.5em", marginTop: "20px" }}
+        >
+          {/* InfoCard */}
+          Try saying: Add income for ₹500 in category Salary for Monday...
+        </Typography>
+        <Divider />
+        <Form />
+      </CardContent>
+      <CardContent className={classes.cartContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            {/* List */}
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default Main;
--- src/index.js
+++ src/index.js
@@ -5,5 +5,3 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-//26:28
